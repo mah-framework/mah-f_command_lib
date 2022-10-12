c752b47f6d3ab4fae30a4ccc05ba681bac1db751
mah-framework <115642942+mah-framework@users.noreply.github.com>
2022-10-12 17:18:21 +0300

    Update command_lib.py

--- command_lib.py
+++ command_lib.py
@@ -1,11 +1,11 @@
 # https:/github.com/C0derByM4H6301
 
-
 a = "hello  serdar   anam"
 def comand_check(text):
+  cmd_list = []
   print(text.split(" "))
   for i in text.split(" "):
-    print(i)
-
+    if i != "" and i and " " and i != '' and i != ' ':
+      cmd_list.append(i)
+  print(cmd_list)
 comand_check(a)
-
