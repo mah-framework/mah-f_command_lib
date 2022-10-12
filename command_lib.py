61021e4a4f317fd855d9d5e7a64fdb021e4e38f6
mah-framework <115642942+mah-framework@users.noreply.github.com>
2022-10-12 17:26:58 +0300

    Update command_lib.py

--- command_lib.py
+++ command_lib.py
@@ -1,6 +1,7 @@
 # https:/github.com/C0derByM4H6301
 
-a = "hello  serdar   anam"
+#a = "hello  serdar   anam"
+a = " set  "
 def comand_check(text):
   cmd_list = []
   print(text.split(" "))
@@ -8,4 +9,14 @@ def comand_check(text):
     if i != "" and i and " " and i != '' and i != ' ':
       cmd_list.append(i)
   print(cmd_list)
+  print(len(cmd_list))
+  len_list = len(cmd_list)
+  if len_list < 1:
+    print("boş string")
+  if len_list == 1:
+    print("birinci komut")
+    if cmd_list[0] == "set":
+      print("set fonksiyonuşimdi çalışacak")
+      if len_list < 2:
+        print("değişken yok")
 comand_check(a)
